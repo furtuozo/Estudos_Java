4fc513cc3953f98318c05125df83579966b612fa
João Pedro Furtuozo <127035956+furtuozo@users.noreply.github.com>
2024-08-29 00:21:29 -0300

    Ex_10.java

--- Arrays/Ex_10.java
+++ Arrays/Ex_10.java
@@ -7,24 +7,16 @@ public class Ex_10 {
         Scanner sc = new Scanner(System.in);
 
         int [] vectA = {1,2,3,4,5,6,7,8,9,10};
-        int [] vectB = {1,2,3,4,5,6,7,8,9,10};
-        double [] vectC = new double[vectA.length];
+        int [] vectB = new int[vectA.length];
 
-        for(int a : vectA){
-            System.out.println(a);
+        for (int i = 0; i < vectA.length; i++) {
+            vectB[i] = vectA[i] % 2;
         }
-        System.out.println("-------------");
+
         for(int b : vectB){
             System.out.println(b);
         }
-        System.out.println("-------------");
 
-        for (int i = 0; i < vectA.length; i++) {
-            vectC[i] = vectA[i] / (vectB[i]);
-        }
-        for (double c : vectC  ){
-            System.out.println(c);
-        }
     sc.close();
     }
 }
